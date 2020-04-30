22d0b4f2f138222dd44b9f97fce13e4bc91b858e
Leandro de Oliveira <leandro4gl@gmail.com>
2020-04-30 18:04:21 -0300

    Projeto Inicial

---
+++ IAM/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IAM.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
