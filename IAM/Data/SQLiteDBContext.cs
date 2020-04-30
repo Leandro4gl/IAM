fdedd59ad5a7563d0a5d8940a4fca07204166b93
Leandro de Oliveira <leandro4gl@gmail.com>
2020-04-30 18:24:25 -0300

    Conexao com Base de Dados SQLite

---
+++ IAM/Data/SQLiteDBContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using IAM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IAM.Data
+{
+    public class SQLiteDBContext : DbContext
+    {
+
+        // Antes de mais nada instalar os pacotes:
+        // Install-Package Microsoft.EntityFrameworkCore.Sqlite
+        // Install-Package Microsoft.EntityFrameworkCore.Tools
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("Data Source=sqlitedemo.db");
+
+        public DbSet<Usuario> Usuario { get; set; }
+
+
+    }
+}
